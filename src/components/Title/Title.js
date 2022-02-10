f42f653b379f217c56e11977a76c1ecca8e93881
Yada Ly <lyyada@gmail.com>
2022-02-10 09:45:13 +0100

    add component Title

---
+++ src/components/Title/Title.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Title extends Component {
+  render() {
+    return <h1 className="apartmentTitle"> {this.props.title}</h1>;
+  }
+}
