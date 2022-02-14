0b0185205bc5f62a424beaa74a73bd0c2a02232c
Yada Ly <lyyada@gmail.com>
2022-02-14 21:19:51 +0100

    add Host component

--- src/components/Host/Host.js
+++ src/components/Host/Host.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
+import "./Host.css";
 
 class Host extends Component {
   render() {
+    const { name, img } = this.props;
+    const firstname = name.split(" ")[0];
+    const lastname = name.split(" ")[1];
+
     return (
-      <div className="apartmentHost">
-        <p className="apartmentNameHost">{this.props.host.name}</p>
-        <img
-          className="apartmentImgHost"
-          alt=""
-          src={this.props.host.picture}
-        />
+      <div className="host">
+        <p>
+          <span>{firstname ? firstname : ""}</span>
+          <span>{lastname ? lastname : ""}</span>
+        </p>
+        <img src={img} alt={name} />
       </div>
     );
   }
